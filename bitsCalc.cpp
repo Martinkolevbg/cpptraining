2b6d612fd2d0011226ab15bb4b9373c9c51efb9e
Martin Kolev <martinkolevbg93@gmail.com>
2017-11-19 18:08:50 +0200

    Fixed structure and tests

--- bitsCalc.cpp
+++ bitsCalc.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
-
 
 int calcBits(int number){
 
@@ -25,57 +23,41 @@ int calcBitsFast(int number) {
     return bits;
 }
 
-
-
 int main()
 {
-    int n=0;
+    int n = 0;
  
     std::cout << "Enter natural number:";
-        std::cin >> n;
+    std::cin >> n;
 
     int result = calcBits(n);
 
-    std::cout << "Your number needs =" << result << " bits to be written in binary."<<endl;
+    std::cout << "Your number needs = " << result << " bits to be written in binary."<<endl;
     
     //testA
-    
-    	int testNum = 5;
-		int testSucc = 3;
-    	
-    
-   		
-		int testResult = calcBitsFast(testNum);
+    int testNum = 5;
+	int testSucc = 3;	
+	int testResult = calcBitsFast(testNum);
 	
 	if (testResult != testSucc) {
-        cout<<"ERROR! There is a mistake with test "<<testNum<<endl;
+        cout<<"ERROR! There is a mistake with test "<< testNum << endl;
     }
     
     //testB
-   
-       	int testNumB = 149;
-		int testSuccB = 8;
-    	
-    
-   		
-		int testResultB = calcBitsFast(testNumB);
+    int testNumB = 149;
+    int testSuccB = 8;
+    int testResultB = calcBitsFast(testNumB);
 	
 	if (testResultB != testSuccB) {
-        cout<<"ERROR! There is a mistake with test "<<testNumB<<endl;
+        cout<<"ERROR! There is a mistake with test "<< testNumB << endl;
     }
-    	//testC
-    	
-       	int testNumC = 148;
-		int testSuccC = 8;
-    	
     
-   		
-		int testResultC = calcBitsFast(testNumC);
+    //testC	
+    int testNumC = 148;
+    int testSuccC = 8;
+    int testResultC = calcBitsFast(testNumC);
 	
 	if (testResultC != testSuccC) {
         cout<<"ERROR! There is a mistake with test "<<testNumC<<endl;
-    }
-    	
-
-
+    }   	
 }
