66b2992f8925a3c182fc087f8d3dfaec4db9adc6
Martin Kolev <martinkolevbg93@gmail.com>
2017-11-19 17:34:11 +0200

    added bits calc alg.

--- bitsCalc.cpp
+++ bitsCalc.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
-int main(){
+int calcBits(int number){
 
-    std::cout<< "hi"<<endl;
+    if (number == 0 || number == 1) {
+        return 1;
+    }
+    
+    return ceil(log2(number));
+}
 
+int main()
+{
+    int n=0;
+ 
+    std::cout << "Enter natural number:";
+    std::cin >> n;
+
+    int result = calcBits(n);
+
+    std::cout << "Your number needs =" << result << " bits to be written in binary."<<endl;
+    
     return 0;
 }
