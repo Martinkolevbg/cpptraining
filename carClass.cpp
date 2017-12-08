d8c6699838422c026a95bc1beda2dda8aa361e94
Martinkolevbg <33600914+Martinkolevbg@users.noreply.github.com>
2017-12-08 15:13:14 +0200

    Added horsePower and reworked car class.

--- carClass.cpp
+++ carClass.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 #include <string>
+#include "carClass.h"
 
 using namespace std;
-
+/*
 class Car {
-    string model;
+    int maxSpeed;
     int voltage;
-    int bitches;
     int rimsSize;
-    int stereo;
+    int horsePower;
+    int bitches;
     string color;
-
+    string make;
 public:
     int currentSpeed = 0;
-    int maxSpeed;
-    
-    Car(string, int, int, string);
     void hitGas();
     void hitBrakes();
+    void hitBrakesHard();
     void showDashboard();
     void showPimpDashboard();
+
+    Car(int ms, int vol, int rSize,int hp,int bitch, string col, string mak){
+        cout << "Car created" << endl << endl;
+        maxSpeed = ms;
+        voltage = vol;
+        rimsSize = rSize;
+        horsePower = hp;
+        color = col;
+        make = mak;
+        bitches = bitch;
+
+    }
 };
-
-Car::Car(string m, int cVoltage, int mSpeed, int bitch)
-{
-    cout << "Car created" << endl;
-    model = m;
-    voltage = cVoltage;
-    maxSpeed = mSpeed;
-    bitches = bitch;
-}
-
+*/
 void Car::hitGas()
 {
     currentSpeed++;
@@ -40,44 +42,41 @@ void Car::hitBrakes()
 {
     currentSpeed--;
 }
+void Car::hitBrakesHard()
+{
+    currentSpeed-5;
+}
 
 void Car::showDashboard()
 {
-    std::cout << "Car model is: " << model << endl;
-    std::cout << "Car max Speed: " << maxSpeed << endl;
-    std::cout << "Car Cable Voltage: " << voltage << endl
+    std::cout << "Car model is: " << make << endl;
+    std::cout << "Maximum Speed: " << maxSpeed << endl;
+    std::cout << "Cable Voltage: " << voltage << endl;
+    std::cout << "Car has: "<<horsePower<<" horse power"<<endl;
+    std::cout << ""
               << endl;
 }
 
 void Car::showPimpDashboard()
 {
-    std::cout << "Car model is: " << model << endl;
-    std::cout << "Car max Speed: " << maxSpeed << endl;
-    std::cout << "Car Cable Voltage: " << voltage << endl;
-    std::cout << "Car Has thouse big rims " << rimsSize << " nigga" << endl;
-    std::cout << "Car collor:"<<carColor
-    std::cout << "Car attracts " << bitches << " BITCHES!!!" << endl
+    std::cout << "This is a real " << make <<", homie."<<endl;
+    std::cout << "You can ride with " << maxSpeed <<" kMh with this beauty"<< endl;
+    std::cout << "Cable Voltage is burning you with: " << voltage <<" volts." << endl;
+    std::cout << "this baby has thouse big rims: "<< rimsSize << endl;
+    std::cout << "and is painted in pure "<<color<<" paint."<< endl;
+    std::cout << "You can attract "<<bitches<<" around you with that baby."<<endl;
+    std::cout << ""
               << endl;
 }
 
-int main(){
-    Car bibiCar("CADILAC", 12 , 170 , 666, "PINK")
-    bibiCar.showPimpDashboard();
-    
-    Car gaviCar("Tesla", 220, 320, 456);
-    gaviCar.currentSpeed = 80;
-    gaviCar.showDashboard();
 
-    while (gaviCar.currentSpeed < 320) {
-        gaviCar.hitGas();
-    }
-    cout << "Gavi car is moving with " << gaviCar.currentSpeed << "kmH" << endl;
+int main()
+{
+    Car bibiCar(220, 12, 17, 150, 5,"Red", "polo");
+    Car gaviCar(360, 220, 20, 500, 9999,"silver","Tesla");
 
-    while (gaviCar.currentSpeed != 50) {
-        gaviCar.hitBrakes();
-    }
+    bibiCar.showDashboard();
+    gaviCar.showPimpDashboard();
 
-    cout << "Gavi car is moving with " << gaviCar.currentSpeed << "kmH" << endl;
-
-    return 0;
+return 0;
 }
